694be2ec909c51655e820d8dc864fa2561d4c4ab
tamilsivakan <167868097+tamilsivakan@users.noreply.github.com>
2024-07-09 14:07:59 +0530

    create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+docker login tamilkgs -p dckr_pat_5I-haTWTm-Ri6wa8_m7pOAYsy3k
+if [[ $GIT_BRANCH == "origin/dev" ]]; then
+       sh 'chmod +x build.sh'
+       sh './build.sh'
+
+        docker tag react-image tamilkgs/dev
+        docker push tamilkgs/dev
+
+if [[ $GIT_BRANCH == "origin/master" ]]; then
+        sh 'chmod +x build.sh'
+        sh './build.sh'
+
+        docker tag react-image tamilkgs/prod
+        docker push tamilkgs/prod
+
+else
+        echo "failed"
+fi
